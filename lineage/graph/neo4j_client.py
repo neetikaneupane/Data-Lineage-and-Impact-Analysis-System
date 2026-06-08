67064a8705a2debbd52a3710c699a6452b2edf49
neetikaneupane <neetikaneupane@gmail.com>
2026-06-08 07:50:30 +0545

    add Neo4j indexes on Column.id, Table.name, and File.path

--- lineage/graph/neo4j_client.py
+++ lineage/graph/neo4j_client.py
@@ -22,3 +22,13 @@ class Neo4jClient:
 
     def clear_all(self):
         self.run("MATCH (n) DETACH DELETE n")
+
+    def create_indexes(self):
+        indexes = [
+            "CREATE INDEX column_id IF NOT EXISTS FOR (c:Column) ON (c.id)",
+            "CREATE INDEX table_name IF NOT EXISTS FOR (t:Table) ON (t.name)",
+            "CREATE INDEX file_path IF NOT EXISTS FOR (f:File) ON (f.path)",
+        ]
+        for idx in indexes:
+            self.run(idx)
+        print("Indexes created.")
--- run_ingest.py
+++ run_ingest.py
@@ -1,9 +1,14 @@
 from lineage.parser.sql_parser import parse_all_sql_files
 from lineage.parser.python_parser import parse_all_python_files
 from lineage.graph.ingester import ingest_all, ingest_python_lineage
+from lineage.graph.neo4j_client import Neo4jClient
 
 parsed_sql = parse_all_sql_files("data/sql_scripts")
 ingest_all(parsed_sql)
 
 parsed_py = parse_all_python_files("data/python_scripts")
 ingest_python_lineage(parsed_py)
+
+client = Neo4jClient()
+client.create_indexes()
+client.close()
